8e20b23bd96c50ed96fb4165eb53552163e337f5
Qiu Di <di.qiu@gree.net>
2013-02-05 17:57:15 +0900

    mv .zshrc to zshrc, add ln-s for zshrc

--- init.sh
+++ init.sh
@@ -3,6 +3,8 @@ ln -s ~/.dotfiles/git/gitignore ~/.gitignore
 
 
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+
 #mv ~/.vim ~/.vim_back
 mkdir ~/.vim
 ln -s ~/.dotfiles/vim/vim/* ~/.vim/
