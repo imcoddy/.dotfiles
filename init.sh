07dd2daf4dca8ca3ca197be65fd13bc3a8b0bf59
Qiu Di <di.qiu@gree.net>
2013-02-08 16:01:29 +0900

    add init for git-completion

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/gitignore ~/.gitignore
-
+ln -s ~/.dotfiles/git/git-completion.bash ~/.git-completion.bash
 
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/zshrc ~/.zshrc
