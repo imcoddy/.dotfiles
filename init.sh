3029e47f3a0fdcc43697068b79e4ef6398f7685a
Qiu Di <di.qiu@gree.net>
2013-02-08 14:38:57 +0900

    add screenrc

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ ln -s ~/.dotfiles/git/gitignore ~/.gitignore
 
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/zshrc ~/.zshrc
+ln -s ~/.dotfiles/screenrc ~/.screenrc
 
 #mv ~/.vim ~/.vim_back
 mkdir ~/.vim
