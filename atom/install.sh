6037a4b005cda782b0c7c1f7da8de72cd9e20503
Qiu Di <di.qiu@gree.net>
2015-04-02 23:15:44 +0900

    add atom config and common packages

---
+++ atom/install.sh
@@ -0,0 +1,15 @@
+apm install atom-beautify
+apm install autocomplete-plus
+apm install autocomplete-snippets
+apm install color-picker
+apm install editor-stats
+apm install emmet
+apm install file-icons
+apm install filetype-color
+apm install jshint
+apm install linter
+apm install markdown-preview
+apm install merge-conflicts
+apm install terminal-status
+apm install todo-show
+apm install vim-mode
