5c7254c1cdd32d3cadc034d6d0d90ce50897d7ac
Qiu Di <di.qiu@gree.net>
2015-06-22 20:59:43 +0900

    Fix creating link of private file

--- karabiner/export.sh
+++ karabiner/export.sh
@@ -3,5 +3,5 @@
 pwd
 cd ~/.dotfiles/karabiner
 /Applications/Karabiner.app/Contents/Library/bin/karabiner export > import.sh
-echo 'ln -sfv ./private.xml ~/Library/Application\ Support/Karabiner/private.xml' >> import.sh
+echo 'ln -sfv ~/.dotfiles/karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml' >> import.sh
 chmod ug+x import.sh
