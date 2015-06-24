89f9346133949ea0d06430e6ca99e18c38c3027f
Qiu Di <di.qiu@gree.net>
2015-06-24 21:07:32 +0900

    Update Karabiner with many options in private.xml, update import.sh

--- karabiner/import.sh
+++ karabiner/import.sh
@@ -6,23 +6,29 @@ $cli set remap.app_term_meta_movement 1
 /bin/echo -n .
 $cli set remap.optionR2optionR_commandSpace 1
 /bin/echo -n .
-$cli set remap.hhkmode 1
+$cli set private.swap_leftcommand_and_leftoption_with_minila 1
+/bin/echo -n .
+$cli set private.filco.app_to_space_command_l 1
 /bin/echo -n .
 $cli set remap.shiftR2shiftR_capslock 1
 /bin/echo -n .
 $cli set option.vimode_fn_hjkl 1
 /bin/echo -n .
-$cli set option.vimode_fn_hjkl_rapid 1
+$cli set remap.hjkl_arrow 1
 /bin/echo -n .
-$cli set remap.commandR2commandR_commandSpace 1
+$cli set private.remap.input_source 1
+/bin/echo -n .
+$cli set remap.space_to_command 1
+/bin/echo -n .
+$cli set option.vimode_fn_hjkl_rapid 1
 /bin/echo -n .
 $cli set option.extra_cursor_optionL_kjhl 1
 /bin/echo -n .
 $cli set remap.controlL2controlL_escape 1
 /bin/echo -n .
-$cli set remap.hhkmode_vi_cursor 1
+$cli set private.appkey_and_numberkey_consumerkey_with_minila 1
 /bin/echo -n .
-$cli set remap.shiftDelete2tilde 1
+$cli set remap.shiftEscape2tilde 1
 /bin/echo -n .
 /bin/echo
 ln -sfv ~/.dotfiles/karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml
