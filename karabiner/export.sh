eaf5e41685fcd363956a5cad29b05df7ccd71f3b
Qiu Di <di.qiu@gree.net>
2015-12-19 16:41:51 +0900

    adjust import.sh to sort by settings, comment out setting for japanese keyboard now

--- karabiner/export.sh
+++ karabiner/export.sh
@@ -2,6 +2,10 @@
 
 pwd
 cd ~/.dotfiles/karabiner
-/Applications/Karabiner.app/Contents/Library/bin/karabiner export > import.sh
+echo '#/bin/sh' > import.sh
+echo 'cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner' >> import.sh
+/Applications/Karabiner.app/Contents/Library/bin/karabiner export > tmp.txt
+cat tmp.txt | sort | grep 'set' >> import.sh
 echo 'ln -sfv ~/.dotfiles/karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml' >> import.sh
 chmod ug+x import.sh
+rm tmp.txt
--- karabiner/import.sh
+++ karabiner/import.sh
@@ -1,58 +1,38 @@
-#!/bin/sh
-
+#/bin/sh
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
-
 $cli set option.extra_cursor_optionL_kjhl 1
-/bin/echo -n .
-$cli set remap.doublepresscommandQ 1
-/bin/echo -n .
-$cli set private.general.control_to_jump_by_word 1
-/bin/echo -n .
-$cli set private.general.control_np_to_updown 1
-/bin/echo -n .
-$cli set remap.optionR2optionR_commandSpace 1
-/bin/echo -n .
-$cli set private.general.control_r_to_option_arrow_right 1
-/bin/echo -n .
-$cli set private.filco.volume_control 1
-/bin/echo -n .
-$cli set remap.hhkmode 1
-/bin/echo -n .
-$cli set option.vimode_fn_hjkl_rapid 1
-/bin/echo -n .
-$cli set remap.hhkmode_vi_cursor 1
-/bin/echo -n .
-$cli set remap.shiftDelete2tilde 1
-/bin/echo -n .
-$cli set private.filco.app_to_space_command_l 1
-/bin/echo -n .
-$cli set private.remap.input_source 1
-/bin/echo -n .
-$cli set private.filco.swap_backquote_and_backslash 1
-/bin/echo -n .
-$cli set private.general.control_esc_to_control_backquote 1
-/bin/echo -n .
-$cli set parameter.holdingkeytokey_wait 400
-/bin/echo -n .
-$cli set private.general.control_v_to_option_arrow_left 1
-/bin/echo -n .
-$cli set remap.controlL2controlL_escape 1
-/bin/echo -n .
-$cli set remap.shiftR2shiftR_capslock 1
-/bin/echo -n .
 $cli set option.vimode_fn_hjkl 1
-/bin/echo -n .
-$cli set remap.app_term_meta_movement 1
-/bin/echo -n .
-$cli set remap.space_to_command 1
-/bin/echo -n .
-$cli set private.filco.swap_command_l_and_option_l 1
-/bin/echo -n .
-$cli set remap.shiftEscape2tilde 1
-/bin/echo -n .
+$cli set option.vimode_fn_hjkl_rapid 1
+$cli set parameter.holdingkeytokey_wait 400
+$cli set private.filco.app_to_space_command_l 1
 $cli set private.filco.forward_delete_to_command_r 1
-/bin/echo -n .
+$cli set private.filco.swap_backquote_and_backslash 1
+$cli set private.filco.swap_command_l_and_option_l 1
+$cli set private.filco.volume_control 1
+$cli set private.general.control_esc_to_control_backquote 1
+$cli set private.general.control_np_to_updown 1
+$cli set private.general.control_r_to_option_arrow_right 1
+$cli set private.general.control_to_jump_by_word 1
+$cli set private.general.control_v_to_option_arrow_left 1
+#$cli set private.japanese.command_r_to_space_command_l 1
+#$cli set private.japanese.shift_underscore_ro_to_shift_backslash 1
+#$cli set private.japanese.underscore_ro_to_backslash 1
+$cli set private.remap.input_source 1
+$cli set remap.app_term_meta_movement 1
+#$cli set remap.backslash2enter 1
+#$cli set remap.commandR2optionR 1
+$cli set remap.controlL2controlL_escape 1
+$cli set remap.doublepresscommandQ 1
+$cli set remap.hhkmode 1
+$cli set remap.hhkmode_vi_cursor 1
 $cli set remap.hjkl_arrow 1
-/bin/echo -n .
-/bin/echo
+#$cli set remap.jis_eisuu2commandL_eisuu 1
+#$cli set remap.jis_eisuu2space 1
+#$cli set remap.jis_jansi 1
+#$cli set remap.jis_kana2optionR_kana 1
+$cli set remap.optionR2optionR_commandSpace 1
+$cli set remap.shiftDelete2tilde 1
+$cli set remap.shiftEscape2tilde 1
+$cli set remap.shiftR2shiftR_capslock 1
+$cli set remap.space_to_command 1
 ln -sfv ~/.dotfiles/karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml
