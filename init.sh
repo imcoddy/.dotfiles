5e023989570cba4e4938231226019007005e7f63
imcoddy <imcoddy@gmail.com>
2019-08-05 19:41:03 +0900

    Add zsh plugin installation

--- init.sh
+++ init.sh
@@ -21,3 +21,7 @@ ln -s ~/.dotfiles/zshrc ~/.zshrc
 mkdir ~/.vim
 ln -s ~/.dotfiles/vim/vim/* ~/.vim/
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
