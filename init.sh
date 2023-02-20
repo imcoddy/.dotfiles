4aa70c4b622bfa31b852e955e19add866ebf9608
imcoddy <imcoddy@gmail.com>
2023-02-20 09:07:34 +0900

    adjust some alias

--- init.sh
+++ init.sh
@@ -22,5 +22,3 @@ ln -s ~/.dotfiles/vim/vim/* ~/.vim/
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-echo 'ZSH_THEME="random"' >> ~/.zshrc
